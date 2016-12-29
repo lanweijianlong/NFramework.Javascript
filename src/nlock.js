3cba6adc036e2a45c81c064f1c0d57b673a45f57
NuClear <hzy19860111@163.com>
2016-12-29 15:41:42 +0800

    add lockSubmit/unlockSubmit

--- src/nlock.js
+++ src/nlock.js
@@ -2,14 +2,19 @@
     window.NF || (window.NF = {});
 
     var DefaultLockName = "DefaultLockName";
-    function getName(name) {
+    function getLockName(name) {
         if (!(name && name != ''))
             name = DefaultLockName;
         return name;
     };
+    function getSubmitLockName(name) {
+        if (!(name && name != ''))
+            name = DefaultLockName;
+        return name + "_Submit";
+    };
     window.NF.NLock = window.NLock = {
         lock: function (name) {
-            var lockName = getName(name);
+            var lockName = getLockName(name);
 
             if (this.Locks[lockName])
                 if (this.Locks[lockName] == true) {
@@ -20,14 +25,27 @@
             this.Locks[lockName] = true;
         },
         isLocked: function (name) {
-            var lockName = getName(name);
+            var lockName = getLockName(name);
 
             return this.Locks[lockName] || false;
         },
         unLock: function (name) {
-            var lockName = getName(name);
+            var lockName = getLockName(name);
 
             this.Locks[lockName] = false;
+        },
+        lockSubmit: function (name) {
+            var lockName = getSubmitLockName(name);
+            if (this.isLocked(lockName)) {
+                console.info("ALready locked,can not submit!");
+                return false;
+            }
+            this.lock(lockName);
+            return true;
+        },
+        unlockSubmit:function(name){
+            var lockName = getSubmitLockName(name);
+            this.unLock(lockName);
         },
 
         Locks: {}
